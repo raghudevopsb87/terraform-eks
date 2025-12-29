bc4c4c5a163d6026db71cfadcc9b876673570f79
Raghu K <raghuk.vit@gmail.com>
2025-12-29 08:13:59 +0530

    Roboshop project

---
+++ main.tf
@@ -0,0 +1,45 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "my-cluster"
+  kubernetes_version = "1.34"
+
+  # addons = {
+  #   coredns                = {}
+  #   eks-pod-identity-agent = {
+  #     before_compute = true
+  #   }
+  #   kube-proxy             = {}
+  #   vpc-cni                = {
+  #     before_compute = true
+  #   }
+  # }
+
+  # Optional
+  endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = "vpc-0f995da5da96d9248"
+  subnet_ids               = ["subnet-0ce8afa0dde986bc0", "subnet-0629872d39e431ea9"]
+  control_plane_subnet_ids = ["subnet-0ce8afa0dde986bc0", "subnet-0629872d39e431ea9"]
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.xlarge"]
+
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+    }
+  }
+
+  tags = {
+    Terraform   = "true"
+  }
+}
