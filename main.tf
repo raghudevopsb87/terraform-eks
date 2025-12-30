162a968202611ae7399b3ced5e9944710c19210e
Raghu K <raghuk.vit@gmail.com>
2025-12-30 07:09:57 +0530

    Roboshop project

--- main.tf
+++ main.tf
@@ -10,31 +10,12 @@ resource "aws_eks_cluster" "main" {
   }
 }
 
-# resource "aws_launch_template" "main" {
-#   name     = "main"
-#
-#   block_device_mappings {
-#     device_name = "/dev/xvda"
-#
-#     ebs {
-#       volume_size = 20
-#       encrypted   = true
-#     }
-#   }
-#
-# }
-
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "main"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = ["subnet-0ce8afa0dde986bc0", "subnet-0629872d39e431ea9"]
   instance_types  = ["t3.xlarge"]
-
-  # launch_template {
-  #   name    = aws_launch_template.main.name
-  #   version = "$Latest"
-  # }
 
   scaling_config {
     desired_size = 1
