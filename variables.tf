7bdea70f3f564341d20c14c3990093530c26f5ec
Raghu K <raghuk.vit@gmail.com>
2026-01-07 06:36:46 +0530

    Roboshop project

--- variables.tf
+++ variables.tf
@@ -2,28 +2,28 @@ variable "env" {
   default = "dev"
 }
 
-variable "ami" {
-  default = "ami-09c813fb71547fc4f"
-}
-
-variable "instance_type" {
-  default = "t3.small"
-}
-
-variable "vpc_security_group_ids" {
-  default = [ "sg-09663d91a4fca31c9" ]
-}
-
-variable "zone_id" {
-  default = "Z057881017RC0RRKVUX8E"
-}
-
-variable "components" {
-  default = {
-    mongodb = ""
-    mysql = ""
-    rabbitmq = ""
-    redis = ""
-  }
-}
+# variable "ami" {
+#   default = "ami-09c813fb71547fc4f"
+# }
+#
+# variable "instance_type" {
+#   default = "t3.small"
+# }
+#
+# variable "vpc_security_group_ids" {
+#   default = [ "sg-09663d91a4fca31c9" ]
+# }
+#
+# variable "zone_id" {
+#   default = "Z057881017RC0RRKVUX8E"
+# }
+#
+# variable "components" {
+#   default = {
+#     mongodb = ""
+#     mysql = ""
+#     rabbitmq = ""
+#     redis = ""
+#   }
+# }
 
