c249ebb334e0b12aa956a806334ca35fa5020a82
Raghu K <raghuk.vit@gmail.com>
2026-01-23 08:34:15 +0530

    Roboshop project

--- addons.tf
+++ addons.tf
@@ -3,37 +3,17 @@ resource "aws_eks_addon" "pod-identity" {
   addon_name   = "eks-pod-identity-agent"
 }
 
+resource "aws_eks_addon" "external-dns" {
+  depends_on = [aws_eks_addon.pod-identity]
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "external-dns"
+}
+
+
 resource "aws_eks_pod_identity_association" "example" {
   cluster_name    = aws_eks_cluster.main.name
   namespace       = "default"
   service_account = "default"
-  role_arn        = aws_iam_role.example.arn
-  #role_arn        = "arn:aws:iam::739561048503:role/test"
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["pods.eks.amazonaws.com"]
-    }
-
-    actions = [
-      "sts:AssumeRole",
-      "sts:TagSession"
-    ]
-  }
-}
-
-resource "aws_iam_role" "example" {
-  name               = "eks-pod-identity-example"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "example_s3" {
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  role       = aws_iam_role.example.name
+  role_arn        = "arn:aws:iam::739561048503:role/test"
 }
 
--- iam.tf
+++ iam.tf
@@ -75,4 +75,49 @@ resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOn
   role       = aws_iam_role.node.name
 }
 
+## External-DNS
+resource "aws_iam_role" "external-dns" {
+  name               = "external-dns"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "pod.eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+  inline_policy {
+    name = "external-dns"
 
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "route53:ChangeResourceRecordSets",
+            "route53:ListResourceRecordSets",
+            "route53:ListTagsForResources"
+          ],
+          "Resource": [
+            "arn:aws:route53:::hostedzone/*"
+          ]
+        },
+        {
+          "Effect": "Allow",
+          "Action": [
+            "route53:ListHostedZones"
+          ],
+          "Resource": [
+            "*"
+          ]
+        }
+      ]
+    })
+  }
+}
