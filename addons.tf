fbbc440aac4cf1654799461ae355f8d3f6788ad6
Raghu K <raghuk.vit@gmail.com>
2026-01-23 08:22:04 +0530

    Roboshop project

--- addons.tf
+++ addons.tf
@@ -3,3 +3,9 @@ resource "aws_eks_addon" "pod-identity" {
   addon_name   = "eks-pod-identity-agent"
 }
 
+resource "aws_eks_pod_identity_association" "example" {
+  cluster_name    = aws_eks_cluster.main.name
+  namespace       = "default"
+  service_account = "default"
+  role_arn        = "arn:aws:iam::739561048503:role/test"
+}
