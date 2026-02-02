27ea24f49006aa45fa885444c676eafdba9f8a8b
Raghu K <raghuk.vit@gmail.com>
2026-02-02 06:37:40 +0530

    Roboshop project

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_eks_node_group" "main" {
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = ["subnet-0ce8afa0dde986bc0", "subnet-0629872d39e431ea9"]
   instance_types  = ["t3.xlarge"]
+  capacity_type   = "SPOT"
 
   scaling_config {
     desired_size = 1
