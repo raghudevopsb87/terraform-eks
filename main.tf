db8bfc34fa72a88a2e2b1e5b22f9302e155cfe83
Raghu K <raghuk.vit@gmail.com>
2026-02-04 07:48:22 +0530

    Roboshop project

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_eks_node_group" "main" {
   capacity_type   = "SPOT"
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     min_size     = 1
     max_size     = 10
   }
